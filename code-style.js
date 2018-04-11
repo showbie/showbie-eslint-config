03e6e0a08fe33e33beac49adc63a541c33010a9d
Jeff <rustyangel@gmail.com>
2018-04-11 10:57:48 -0600

    🎁 Add eslint rules

---
+++ code-style.js
@@ -0,0 +1,12 @@
+/**
+ * Enforce a consistent code style.
+ */
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    rules: {
+        semi: ['error', 'always'],
+    },
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        ecmaFeatures: { experimentalObjectRestSpread: true },
+    },
+    env: {
+        browser: true,
+    },
+    extends: ['eslint:recommended', './code-style.js'],
+    rules: {},
+};
