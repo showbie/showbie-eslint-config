090c258a442771c80919eb07de1d649a226e214c
Jeff <rustyangel@gmail.com>
2018-07-18 11:12:52 -0600

    Get more strict with ember rules & add override for test files

--- ember.js
+++ ember.js
@@ -7,13 +7,19 @@ module.exports = {
   },
   extends: ['plugin:ember/recommended'],
   rules: {
-    'ember/no-empty-attrs': 'warn',
-    'ember/no-observers': 'warn',
-    'ember/use-ember-get-and-set': 'warn',
-
+    'ember/no-empty-attrs': 'error',
+    'ember/no-observers': 'error',
     'ember/order-in-components': 'error',
     'ember/order-in-controllers': 'error',
     'ember/order-in-models': 'error',
     'ember/order-in-routes': 'error',
+    'ember/use-ember-get-and-set': 'error',
   },
+  overrides: [
+    // test files; ignore the ember-get rule
+    {
+      files: ['tests/**/*.js'],
+      rules: { 'ember/use-ember-get-and-set': 'off' },
+    },
+  ],
 };
