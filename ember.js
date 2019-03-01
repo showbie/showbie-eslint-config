e349b8059d7b228474ff87c67a09383abbf502a7
Jeff <rustyangel@gmail.com>
2019-03-01 10:36:33 -0700

    Update ember config overrides

--- ember.js
+++ ember.js
@@ -2,10 +2,11 @@
  * Ember-specific rules.
  */
 module.exports = {
+  plugins: ['ember'],
+  extends: ['plugin:ember/recommended'],
   env: {
     browser: true,
   },
-  extends: ['plugin:ember/recommended'],
   rules: {
     'ember/no-empty-attrs': 'error',
     'ember/order-in-components': 'error',
@@ -15,6 +16,41 @@ module.exports = {
     'ember/use-ember-get-and-set': 'error',
   },
   overrides: [
+    // node files
+    {
+      files: [
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js',
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**',
+      ],
+      parserOptions: {
+        ecmaVersion: 2015,
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['node'],
+      rules: Object.assign(
+        {},
+        require('eslint-plugin-node').configs.recommended.rules,
+        {
+          // without this, CI breaks when running pre-commit without installing Node dependencies
+          'node/no-missing-require': 'off',
+        }
+      ),
+    },
+
     // test files; ignore the ember-get rule
     {
       files: ['tests/**/*.js'],
